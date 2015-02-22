8b0b37a82c12922fe7ed2ad029c9da8fd20b64f8
gerben540 <gerben_timmerman@hotmail.com>
2015-02-22 08:24:19 -0800

    Flag_color betere versie

---
+++ flag_color.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+# Gerben Timmerman
+# flag_color.py
+
+from PyQt4 import QtCore, QtGui
+from random import randrange
+
+class FlagColor(QtGui.QColor):
+	"""Class waarin de kleur wordt bepaald """
+	
+	def __init__(self, land):
+		""" Constructor """
+		super(FlagColor, self).__init__()
+		self.kleur()
+		
+	def initUI(self):
+		self.land = land
+		self.kleur = self.Kleur()
+		
+	def Kleur(self):
+		self.setGreen(randrange(256))
+		self.setRed(randrange(256)
+		self.setBlue(randrange(256)
+		
+		return self
+
+
+def main():
+	app = QtGui.QApplication(sys.argv)
+	color = FlagColor()
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	main()
